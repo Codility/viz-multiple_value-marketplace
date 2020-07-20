c5cb2fd283a9c4f90a6078f85810c1882daa96f3
Paweł Sierszeń <paul@codility.com>
2020-07-20 10:02:10 +0200

    Change default orientation to horizontal

--- src/multiple_value/multiple_value_container.js
+++ src/multiple_value/multiple_value_container.js
@@ -24,7 +24,7 @@ const baseOptions = {
       {'Vertical': 'vertical'},
       {'Horizontal': 'horizontal'}
     ],
-    default: 'auto',
+    default: 'horizontal',
     order: 0,
     display_size: 'half'
   }
